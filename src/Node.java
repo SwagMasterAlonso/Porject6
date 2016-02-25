433f9e90232045e65b627018e3b1583a5b1e5733
James Chow <dragonmaster510@gmail.com>
2016-02-24 22:34:41 -0500

    Began Node class.

--- src/Node.java
+++ src/Node.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
 Alonso
 */
 public class Node {
+	int id;
+	List<Node> edges;
+	double[][] cpt;
 
+	Node(int id, double[][] cpt) {
+		this.edges = new ArrayList<Node>();
+		this.id = id;
+		this.cpt = cpt;
+	}
 }
---
+++ src/VariableType.java
@@ -0,0 +1,4 @@
+
+public enum VariableType {
+	UNKNOWN, QUERY, EVIDENCE;
+}
