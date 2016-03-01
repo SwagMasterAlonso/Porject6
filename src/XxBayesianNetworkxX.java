083e009db568e8c660ea051bad24d0633be0c102
alonso_martinez <almartnz@gmail.com>
2016-03-01 18:32:50 -0500

    Added comments

--- src/XxBayesianNetworkxX.java
+++ src/XxBayesianNetworkxX.java
@@ -15,7 +15,7 @@ public class XxBayesianNetworkxX {
 		String fileName = null;
 		String queryFile = null;
 		int samples =0;
-		
+
 		//get command line arguments, there should only be 2
 		if(args.length==3){
 			String inputString = args[0];
@@ -35,21 +35,21 @@ public class XxBayesianNetworkxX {
 		//doubles to hold the variances from the tests
 		double meanRejV = 0.000;
 		double meanLikeV = 0.000;
-		
+
 		//create our new Bayesian Network
 		Network BayesNet = new Network();
 
 		//creates all of the nodes from the file and adds them to our Bayesian Net
 		createNodes(fileName,BayesNet);
-		
+
 		//adds all of the cpts to the nodes in our file
 		populateNodes(fileName,BayesNet);
 
 		//assigns the type (query, evidence, neither) to our nodes
 		assignStatus(queryFile, BayesNet);
-		
 
-//CODE TO RUN TESTS FOR THE GIVEN SAMPLES
+
+		//CODE TO RUN TESTS FOR THE GIVEN SAMPLES
 		for(int i = 0; i < 10; i++){
 
 			meanRej += rejectionSampling(samples,BayesNet);
@@ -70,15 +70,10 @@ public class XxBayesianNetworkxX {
 		meanRejV/=10;
 		meanLikeV/=10;
 
-
-
 		System.out.println("Mean of Rejection sampling is: "+meanRej);
 		System.out.println("Mean of Likelihood weighting is: "+meanLike);
 		System.out.println("Variance of Rejection sampling is: "+meanRejV);
 		System.out.println("Variance of Likelihood weighting is: "+meanLikeV);
-
-
-
 	}
 
 
@@ -89,7 +84,7 @@ public class XxBayesianNetworkxX {
 
 
 
-//function generates all of then nodes in the given file and adds them to our bayesian network
+	//function generates all of then nodes in the given file and adds them to our bayesian network
 	public static void createNodes(String fileName,Network bayesNet){
 		String line;
 		BufferedReader br = null;
@@ -115,16 +110,14 @@ public class XxBayesianNetworkxX {
 	}
 
 
-	
-	//takes our 
+
+	//takes our nodes and adds the CPTS to them and also adds the parents
 	public static void populateNodes(String fileName,Network bayesNet){
 
-		//	System.out.println("In populate");
 		String line;
 		BufferedReader br = null;
 		try {
 
-			//		System.out.println("Trying");
 			br = new BufferedReader(new FileReader(fileName));
 			while ((line = br.readLine()) != null) {
 				String[] fields = line.split(" ");
@@ -140,58 +133,16 @@ public class XxBayesianNetworkxX {
 				getNode(fields[0],bayesNet).createCPT(probabilities);
 			}
 		}catch(Exception e){};
-
-
-
-
-
-
-
-
-
 	}
 
 
-	public static double[][] getCPT(int size,String[] probs){
-
-		double[][] cpt = null;
-
-		switch(size) {
-		case 2:
-			cpt = new double[1][2];
-			cpt[0][0] = Double.parseDouble(probs[0]);
-			cpt[0][1] = Double.parseDouble(probs[1]);
-		case 4:
-			cpt = new double[2][2];
-			for (int i = 0; i < 2; i++) {
-				for (int j = 0; j < 2; j++) {
-					for(int k = 0; k < size;k++){
-						cpt[i][j] = Double.parseDouble(probs[k]);
-					}
-				}
-			}
-		case 8:
-			cpt = new double[4][2];
-			for (int i = 0; i < 4; i++) {
-				for (int j = 0; j < 2; j++) {
-					for(int k = 0; k < size;k++){
-						cpt[i][j] = Double.parseDouble(probs[k]);
-					}
-				}
-			}
-		default:
-			System.out.println("This array is not valid size.");
-			break;
-		}
-
-		return cpt;
-	}
+	//function returns the parent of the given node in the data file
 	public static String getParentData(String string){
 		return string.substring(string.indexOf('[')+1,string.indexOf(']'));
 	}
 
 
-
+	//helper function to return the node in the arraylist
 	public static Node getNode(String nodeString,Network bayesNet){
 
 		for(Node node: bayesNet.getBayesNetNodes()){
@@ -201,38 +152,12 @@ public class XxBayesianNetworkxX {
 
 			}
 		}
-		// System.out.println("Returning null");
 		return null;
-
-
-
-
-	}
-
-
-	public static void printProbabilities(String fileName,Network bNet){
-		String line;
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader(fileName));
-			while ((line = br.readLine()) != null) {
-				//System.out.println("Reading");
-				//String[] fields = line.split(" ");
-				//System.out.println(fields[2]);
-				System.out.println("Line is: "+line);
-
-				String[] probabilities = getProbs(line).split(" ");
-
-
-
-				//System.out.println("Probs are " +probabilities);
-
-			}
-		}catch(Exception e){};
 	}
 
 
 
+	//assigns the status of each node based off of the query file
 	public static void assignStatus(String fileName, Network bNet) {
 		String line;
 		BufferedReader br = null;
@@ -276,38 +201,32 @@ public class XxBayesianNetworkxX {
 
 	}
 
+
+	//gets the probability data from the given file or the given node
 	public static String getProbs(String string){
-		//	System.out.println("Here");
 		string = string + "*";
 		string = string.replace(".", ",");
-		//	System.out.println(string);
 		int place1 = string.indexOf(",")-1;
-		//	System.out.println("Place 1 is: "+place1);
 		int place2 = string.indexOf("*")-1;
-		//	System.out.println("Place 2 is: "+place2);
-
 		String string2 = string.substring(place1, place2);
 		string2 = string2.replace(",", ".");
-
-		//System.out.println("SubString is " + string2);
 		return string2;
 	}
 
-
+	//returns the type of query that the node is (evidence, query, neither)
 	public static int queryIndex(Boolean[] event,Network bNet){
 
 		for(int i = 0; i < bNet.getBayesNetNodes().size();i++){
-
-
 			if(bNet.getBayesNetNodes().get(i).getType() == VariableType.QUERY){
 				return i;
-
 			}
 		}
 		return -1;
 	}
 
 
+	//function for rejection sampling. Takes the number of samples and the bayesian net
+	//and performs rejection sampling
 	public static double rejectionSampling(int numSamples, Network bNet) {
 
 		int nonRejected = 0;
@@ -320,6 +239,7 @@ public class XxBayesianNetworkxX {
 			event = prior_sample(bNet);
 
 
+			//if is consistent, then do
 			if(checkConsistency(event,bNet)){
 				nonRejected++;
 
@@ -340,13 +260,12 @@ public class XxBayesianNetworkxX {
 
 		vector[0] /= nonRejected;
 		vector[1] /= nonRejected;
-		//		System.out.println("Non Rejected is: " +nonRejected);
-		//		System.out.println("Vector 0 is: "+vector[0]);
-		//		System.out.println("Vector 1 is: "+vector[1]);
 		return vector[0];
 	}
 
 
+	//function computes likelihood_weighting with the number of samples
+	//and the bayesian network and return the probability of being true
 	public static double likelihood_weighting (int numSamples, Network bNet) {
 		double sumAllWeights = 0;
 		double[] vector = {0,0};
@@ -368,12 +287,12 @@ public class XxBayesianNetworkxX {
 
 		vector[0] /= sumAllWeights;
 		vector[1] /= sumAllWeights;
-		//		System.out.println("Sum is: " +sumAllWeights);
-		//		System.out.println("Vector 0 is: "+vector[0]);
-		//		System.out.println("Vector 1 is: "+vector[1]);
+	
 		return vector[0];
 	}
 
+	
+	//checks to make sure that the sample is consistent with the given evidence
 	private static Boolean checkConsistency(Boolean[] event, Network bNet){
 		for(int i = 0; i < event.length;i++){
 			if(bNet.getBayesNetNodes().get(i).getType() == VariableType.EVIDENCE){
@@ -386,7 +305,7 @@ public class XxBayesianNetworkxX {
 		return true;
 	}
 
-
+	//performs prior sampling on the data and return the probability of the parent.
 	private static Boolean[] prior_sample (Network bNet) {
 		Boolean[] event = new Boolean[bNet.getBayesNetNodes().size()];
 		double rndNum = 0.00;
@@ -485,6 +404,7 @@ public class XxBayesianNetworkxX {
 		return event;
 	}
 
+	//performs prior sampling on the data and return the probability of the parent.
 	private static Boolean computeParent(Random rnd, Node n) {
 		double random = 0.00;
 		Boolean result, result2;
@@ -543,6 +463,8 @@ public class XxBayesianNetworkxX {
 		}
 	}
 
+	//function performs a weighted sample on the bayesian network and returns
+	//an object called weight that holds the weight and the sample
 	public static WeightedSample weighted_sample(Network bNet) {
 		Random rnd = new Random();
 		Boolean[] event = new Boolean[bNet.getBayesNetNodes().size()];
@@ -563,6 +485,7 @@ public class XxBayesianNetworkxX {
 		return sample;
 	}
 
+	//function that samples the bayesian network with a given seed, event, and node
 	private static Boolean[] sample (Random seed, Boolean[] event, Node n, int index) {
 		double rndNum = 0.00;
 		Node temp;
@@ -627,7 +550,7 @@ public class XxBayesianNetworkxX {
 		return event;
 	}
 
-
+	//function computes the probability of the node
 	private static double computeProb (Random seed, Node n) {
 		Boolean parent1, parent2;
 
@@ -682,6 +605,9 @@ public class XxBayesianNetworkxX {
 			}
 		}
 	}
+	
+	
+	//function initializes the event and checks to make sure that is evidence
 	private static Boolean[] initializeEvent(Boolean[] event, Network bNet) {
 		for (int i = 0; i < bNet.getBayesNetNodes().size(); i++) {
 			if (bNet.getBayesNetNodes().get(i).getType() == VariableType.EVIDENCE) {
